51455ef150f64c9b1c3a7ae568e4ea2f1c91af43
devmessias <messias.physics@gmail.com>
2019-09-10 09:23:49 -0300

    Create documentation files

--- doi2bib/crossref.py
+++ doi2bib/crossref.py
@@ -1,3 +1,9 @@
+"""
+crossref integration
+===========================
+The core module
+"""
+
 from __future__ import unicode_literals, print_function, absolute_import
 from builtins import str
 import requests
@@ -10,9 +16,12 @@ def get_bib(doi):
     """
     Parameters
     ----------
+
         doi: str
+
     Returns
     -------
+
         found: bool
         bib: str
     """
@@ -31,8 +40,10 @@ def get_json(doi):
     Parameters
     ----------
         doi: str
+
     Returns
     -------
+
         found: bool
         item: dict
             Response from crossref
@@ -51,11 +62,13 @@ def get_bib_from_doi(doi, abbrev_journal=True):
     """
     Parameters
     ----------
+
         doi: str
         abbrev_journal: bool
             If True try to abbreviate the journal name
     Returns
     -------
+
         found: bool
         bib: str
             The bibtex string
