dea16b4490db973d553fe75cbf8d87806407833e
devmessias <messias.physics@gmail.com>
2020-07-16 18:41:28 -0300

    Fix version number

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-readme = open('README','r')
+readme = open('README', 'r')
 README_TEXT = readme.read()
 readme.close()
 
 setup(
     name="doi2bib",
     version="0.4.0",
-    packages=find_packages(exclude=["build",]),
+    packages=find_packages(exclude=["build", ]),
     scripts=["doi2bib/bin/doi2bib"],
     long_description=README_TEXT,
     install_requires=["requests", "future", "bibtexparser"],
@@ -16,7 +16,7 @@ setup(
     description="Generate a bibtex given a doi",
     author="Bruno Messias",
     author_email="messias.physics@gmail.com",
-    download_url="https://github.com/bibcure/doi2bib/archive/0.3.0.tar.gz",
+    download_url="https://github.com/bibcure/doi2bib/archive/0.4.0.tar.gz",
     keywords=["bibtex", "science", "scientific-journals"],
 
     classifiers=[
